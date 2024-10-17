6bb9310e7aa3a10b011bffdafbed401aeafefddb
Ruichimi <theloyworkmail@gmail.com>
2024-10-18 00:06:17 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <ExtremeEditor/>
+</template>
+
+<script>
+import ExtremeEditor from './components/ExremeEditor.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ExtremeEditor
+  }
+}
+</script>
+
+<style>
+#app {
+  background-color: #303030;
+  margin: 0 !important;
+  padding: 0;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #303030;
+}
+</style>
